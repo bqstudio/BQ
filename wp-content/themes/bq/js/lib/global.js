b6d55a97581cde211db6c66a112ce3225bee4303
bqstudio <116818622+bqstudio@users.noreply.github.com>
2023-02-25 11:17:24 -0300

    js

--- wp-content/themes/bq/js/lib/global.js
+++ wp-content/themes/bq/js/lib/global.js
@@ -1,21 +1,21 @@
 jQuery.noConflict();
 jQuery(document).ready(function($){
 
-	AOS.init({ //TODO://AOS
-		duration: 800,
-		easing: 'ease-in-sine',
-	});
+// 	AOS.init({ //TODO://AOS
+// 		duration: 800,
+// 		easing: 'ease-in-sine',
+// 	});
 
 	$(window).scroll(function(){
 		$(window).scrollTop() >= 5 ? $('body').addClass('scrolled') : $('body').removeClass('scrolled');
 	});
 
-	$('a[href^="#"]').on('click', function(e){
-		e.preventDefault();
-		var $href   =   $(this).attr('href');
-		$($href).length && $($href).scrollTo(( $(this).data('scrolldiff') ? $(this).data('scrolldiff') : 0 ), $(this).data('duration'));
-		return false;
-	});
+// 	$('a[href^="#"]').on('click', function(e){
+// 		e.preventDefault();
+// 		var $href   =   $(this).attr('href');
+// 		$($href).length && $($href).scrollTo(( $(this).data('scrolldiff') ? $(this).data('scrolldiff') : 0 ), $(this).data('duration'));
+// 		return false;
+// 	});
 
 	$(".js-go-top").on('click', function() {
 		$([document.documentElement, document.body]).animate({
